a1a765a72fdfc439c8be2ddee4dd74bc3a03ab12
Parth Pawar <parth290601@rediffmail.com>
2021-10-05 12:49:06 +0530

    Added login route

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,12 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { auth } = require("../middleware/auth");
 
-const { register, getUsers, getUserById } = require("../controllers/userController");
+const { register, getUsers, getUserById, login } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/signup", asyncHandler(register));
+router.post("/login", asyncHandler(login));
 
 router.get("/", auth, asyncHandler(getUsers));
 router.get("/:id", auth, asyncHandler(getUserById));
