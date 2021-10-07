6511c926a30c04a28e5909d8bac8d4113ee3d39a
Parth Pawar <parth290601@rediffmail.com>
2021-10-07 11:11:02 +0530

    Added new route

--- routes/post.js
+++ routes/post.js
@@ -2,11 +2,12 @@ const express = require("express");
 const asyncHandler = require("express-async-handler");
 const { auth } = require("../middleware/auth");
 
-const { createPost, deletePost, updatePost, getUserPosts } = require("../controllers/postController");
+const { createPost, deletePost, updatePost, getUserPosts, getAllPosts } = require("../controllers/postController");
 
 const router = express.Router();
 
 router.get("/", auth, asyncHandler(getUserPosts));
+router.get("/all-posts", auth, asyncHandler(getAllPosts));
 router.put("/:id", auth, asyncHandler(updatePost));
 
 router.post("/create-post", auth, asyncHandler(createPost));
